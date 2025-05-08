e7196f4916bd7a979c28c34fd0b8df5b15643a67
Jordan Powell <jordan@jpdesigning.com>
2025-05-08 14:47:48 -0400

    update formatting

--- apps/store-e2e/src/cart.spec.ts
+++ apps/store-e2e/src/cart.spec.ts
@@ -1,32 +1,32 @@
-import { expect, test } from "@playwright/test";
+import { expect, test } from '@playwright/test';
 
 test('can add product to cart', async ({ page }) => {
-    await page.goto('/products/1');
-    await page.click('button:has-text("Add to Cart")');
-    
-    const navItems = page.locator('nav ul li');
-    await navItems.nth(2).click();
-  
-    await expect(page.locator('h1')).toContainText('Cart');
-    await expect(page.locator('text=No items in cart')).toBeHidden();
-    await expect(page.locator('text=Total: $')).toBeVisible();
-  });
-  
-  test('can remove product from cart', async ({ page }) => {
-    await page.goto('/products/1');
-    await page.click('button:has-text("Add to Cart")');
-    const navItems = page.locator('nav ul li');
-    await navItems.nth(2).click();
-    await page.click('button:has-text("X")');
-    await expect(page.locator('text=No items in cart')).toBeVisible();
-    await expect(page.locator('text=Total: $')).toBeHidden();
-  });
-  
-  test('can clear cart', async ({ page }) => {
-    await page.goto('/products/1');
-    await page.click('button:has-text("Add to Cart")');
-    const navItems = page.locator('nav ul li');
-    await navItems.nth(2).click();
-    await page.click('button:has-text("Clear Cart")');
-    await expect(page.locator('text=No items in cart')).toBeVisible();
-  });
+  await page.goto('/products/1');
+  await page.click('button:has-text("Add to Cart")');
+
+  const navItems = page.locator('nav ul li');
+  await navItems.nth(2).click();
+
+  await expect(page.locator('h1')).toContainText('Cart');
+  await expect(page.locator('text=No items in cart')).toBeHidden();
+  await expect(page.locator('text=Total: $')).toBeVisible();
+});
+
+test('can remove product from cart', async ({ page }) => {
+  await page.goto('/products/1');
+  await page.click('button:has-text("Add to Cart")');
+  const navItems = page.locator('nav ul li');
+  await navItems.nth(2).click();
+  await page.click('button:has-text("X")');
+  await expect(page.locator('text=No items in cart')).toBeVisible();
+  await expect(page.locator('text=Total: $')).toBeHidden();
+});
+
+test('can clear cart', async ({ page }) => {
+  await page.goto('/products/1');
+  await page.click('button:has-text("Add to Cart")');
+  const navItems = page.locator('nav ul li');
+  await navItems.nth(2).click();
+  await page.click('button:has-text("Clear Cart")');
+  await expect(page.locator('text=No items in cart')).toBeVisible();
+});
