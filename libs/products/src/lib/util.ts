83bb9cadf3297f8734d91ca58678708379309099
Jordan Powell <jordan@jpdesigning.com>
2025-05-09 09:53:50 -0400

    move some utils to utils lib

--- libs/products/src/lib/util.ts
+++ libs/products/src/lib/util.ts
@@ -1,9 +1,10 @@
 import { TEST_NAME_2 as testName2, TEST_NAME_3 as testName3 } from './util2';
+import { TEST_NAME_4 as testName4, TEST_NAME_5 as testName5, TEST_NAME_6 as testName6, TEST_NAME_7 as testName7 } from '@nx-bitovi/utils';
 
 export const TEST_NAME = 'test name 1';
 export const TEST_NAME_2 = testName2;
 export const TEST_NAME_3 = testName3;
-export const TEST_NAME_4 = 'test name 4';
-export const TEST_NAME_5 = 'test name 5';
-export const TEST_NAME_6 = 'test name 6';
-export const TEST_NAME_7 = 'test name 7';
+export const TEST_NAME_4 = testName4;
+export const TEST_NAME_5 = testName5;
+export const TEST_NAME_6 = testName6;
+export const TEST_NAME_7 = testName7;
--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -6,3 +6,8 @@ export const truncate = (text: string, maxLength = 50) => {
 export const formatPrice = (price: number) => {
     return `$${price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
 };
+
+export const TEST_NAME_4 = 'test name 4';
+export const TEST_NAME_5 = 'test name 5';
+export const TEST_NAME_6 = 'test name 6';
+export const TEST_NAME_7 = 'test name 7';
